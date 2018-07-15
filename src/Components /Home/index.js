dbb509b9a01184830ceea70db15fd1185357109c
dwinatech <diwina1993@hotmail.com>
2018-07-15 20:53:28 +0100

    updated home component to work at two different mode user and admin

--- src/Components /Home/index.js
+++ src/Components /Home/index.js
@@ -6,34 +6,81 @@ import EditCake from '../Cakes/EditCake';
 import DeleteCake from '../Cakes/DeleteCake';
 import AddCake from '../Cakes/AddCake';
 import './home.css';
+import SingleCake from '../Cakes/SingleCake';
 
 class Home extends Component {
+
+    state = {
+        yumFactor: 1,
+        user: 'customer'
+    }
     async componentDidMount(){
         this.props.getListOfCakes();
     }
 
-    render() {
+    handleSelectCakeNumber = (e) => {
+        this.setState({ yumFactor: e.target.value })
+    }
+
+    handleUserType = (e) => {
+        this.setState({ user: e.target.value })
+    }
+
+    render() {   
+        const { user } = this.state;
         const cakes = this.props.listOfCakes.listOfCakes ? this.props.listOfCakes.listOfCakes.data : [];
         return (
-        <div className="App">
-            <h1>My favorite cakes</h1>
-            <AddCake />
-            {
-                cakes.map(cake => (
-                    <Fragment key={cake._id}>
-                        <h1>{cake.name}</h1>
-                        <label>
-                            {cake.yumFactor}
-                        </label>
-                        <br/>
-                        <img src='http://placehold.it/300x200' alt='img' /><br/>
-                        <button className="select-cake" type="submit" >Select</button>
-                        <EditCake {...cake} />
-                        <br/>
-                        <DeleteCake />
-                    </Fragment>
-                ))
-            }
+        <div className="cake-list">
+            <h1>Cakes list</h1>
+            <div className="cakes-number">
+                <label>Select user type</label>
+                <select onChange={this.handleUserType} name="user" >
+                    <option value="customer">Customer</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </div>
+            { user === 'admin' ? <AddCake /> : null }
+            <div className="content">
+                {
+                    cakes.map(cake => (
+                        <div key={cake._id} className="cake-card">
+                            <h1>{cake.name}</h1>
+                            { user !== 'admin' ?
+                                (
+                                    <div className="cakes-number">
+                                        <label>Select number of cakes</label>
+                                        <select onChange={this.handleSelectCakeNumber}>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                            <option value="4">Four</option>
+                                            <option value="5">Five</option>
+                                        </select>
+                                    </div>
+                                ): null
+                            }
+                            {cake.yumFactor ? <label>Number of selected cakes { cake.yumFactor }</label>: null}
+                            <img src={`${cake.imageUrl}`} alt='img' /><br/>
+                            {
+                                user === 'admin' ? (
+                                    <Fragment>
+                                        <SingleCake cake={cake} />
+                                        <EditCake {...cake} cakeId={cake._id} /><br/>
+                                        <DeleteCake cakeId={cake._id} />
+                                    </Fragment>
+                                ):(
+                                    <Fragment>
+                                        <SingleCake cake={cake} />
+                                        <button className="select-cake" type="submit" >Select</button><br/>
+                                    </Fragment>
+                                )
+                            }
+                            
+                            
+                        </div>
+                    ))
+                }
+            </div>
         </div>
         );
     }
