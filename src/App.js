30db369352bcfd86b350c68a53d0ac189adb7498
dwinatech <diwina1993@hotmail.com>
2018-07-22 21:30:33 +0100

    added edit, add and delete routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FavoriteCakes from './Components /Cakes/FavoriteCakes';
 import Home from './Components /Home';
 import './App.css';
-
+import EditCake from './Components /Cakes/EditCake';
+import AddCake from './Components /Cakes/AddCake';
+import DeleteCake from './Components /Cakes/DeleteCake';
 
 class App extends Component {
   render() {
@@ -11,6 +13,10 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route  exact path="/" component={Home} />
+          <Route  exact path="/cakes/edit/:data" component={EditCake} />
+          <Route  exact path="/cakes/add" component={AddCake} />
+          <Route exact path="/cakes/delete/:cakeId" component={DeleteCake} />
+          <Route exact path="/favoritecakes/delete/:favoriteCakeId" component={DeleteCake} />
           <Route  exact path="/favoritecakes" component={FavoriteCakes} />
         </div>
       </Router>
