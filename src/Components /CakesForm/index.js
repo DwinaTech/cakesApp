d267046848a5c40247d81423eed23e3e9417d32a
dwinatech <diwina1993@hotmail.com>
2018-07-24 11:44:34 +0100

    edited editCake style

--- src/Components /CakesForm/index.js
+++ src/Components /CakesForm/index.js
@@ -6,8 +6,8 @@ import './cakesform.css'
 export function CakesForm(props) {    
     return (
         <div className="content">
-            <h1>{ props.edit ? 'Edit' : 'Add new'} cake</h1>
             <div className="form">
+                <h1>{ props.edit ? 'Edit' : 'Add new'} cake</h1>
                 <input value={ props.name } onChange={ props.handleCakeChange } type="text" placeholder="Cake name" name="name" />
                 <input value={ props.imageUrl } onChange={ props.handleCakeChange } type="text" placeholder="Cake image url" name="imageUrl" />
                 <textarea value={props.comment} onChange={ props.handleCakeChange } placeholder="image description"  rows="4" name="comment" />
